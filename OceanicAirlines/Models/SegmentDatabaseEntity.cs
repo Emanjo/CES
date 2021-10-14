3b2fbbb726efc2cc67018bca48c80fef053c5652
Emil André Johannessen <emilaj@netcompany.com>
2021-10-14 18:47:54 +0200

    Renamed and moved models

---
+++ OceanicAirlines/Models/SegmentDatabaseEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace OceanicAirlines.Models
+{
+    public class SegmentDatabaseEntity
+    {
+        public City StartCity { get; }
+        public City EndCity { get; }
+
+        public SegmentDatabaseEntity(City startCity, City endCity)
+        {
+            StartCity = startCity;
+            EndCity = endCity;
+        }
+    }
+}
