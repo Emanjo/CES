4154c54eb8668a09a2eb7b06afbed6ac59e9907c
Daniel Stefaniak Nielsen <dsn@netcompany.com>
2021-10-14 21:27:05 +0200

    Most pages done

---
+++ OceanicAirlines/Pages/Shared/Class.cs
@@ -0,0 +1,9 @@
+﻿namespace OceanicAirlines.Pages
+{
+    public class routeDTO
+    {
+        public int ID { get; set; }
+        public double Cost { get; set; }
+        public double Duration { get; set; }
+    }
+}
