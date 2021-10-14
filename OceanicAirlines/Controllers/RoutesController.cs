550a77aa899b87bfd15d6a77e030b1c1f1df34ef
Emil André Johannessen <emilaj@netcompany.com>
2021-10-14 10:46:50 +0200

    Added controller for the routes endpoint. Also added som mockdata.

---
+++ OceanicAirlines/Controllers/RoutesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OceanicAirlines.Models;
+using System.Collections.Generic;
+
+namespace OceanicAirlines.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoutesController : ControllerBase
+    {
+        [HttpGet]
+        public List<Segment> Get()
+        {
+            return new List<Segment>
+            {
+                new Segment {
+                    EndCity = "Fredrikstad",
+                    StartCity = "Oslo",
+                    Cost = 20,
+                    Time = 2
+                },
+                new Segment {
+                    EndCity = "Copenhagen",
+                    StartCity = "Helsingborg",
+                    Cost = 200,
+                    Time = 1
+                }
+            };
+        }
+    }
+}
---
+++ OceanicAirlines/Models/Segment.cs
@@ -0,0 +1,11 @@
+﻿namespace OceanicAirlines.Models
+{
+    public class Segment
+    {
+        public string StartCity { get; set; }
+        public string EndCity { get; set; }
+        public double Cost { get; set; }
+        public double Time { get; set; }
+        public double? MaxWeigth { get; set; }
+    }
+}
