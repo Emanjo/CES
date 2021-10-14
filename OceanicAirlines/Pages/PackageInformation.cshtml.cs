5776c8df1b54c4bf7a73849981381c2c3b1a077d
Daniel Stefaniak Nielsen <dsn@netcompany.com>
2021-10-14 10:30:18 +0200

    Added pages

---
+++ OceanicAirlines/Pages/PackageInformation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OceanicAirlines.Pages
+{
+    public class PackageInformationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ OceanicAirlines/Pages/SelectRoute.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OceanicAirlines.Pages
+{
+    public class SelectRouteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
