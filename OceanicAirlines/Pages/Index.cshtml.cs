6fc1e5b3bc6e7762f95e74e1005f55b6afa043f1
Daniel Stefaniak Nielsen <dsn@netcompany.com>
2021-10-14 10:13:10 +0200

    initial push

---
+++ OceanicAirlines/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OceanicAirlines.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult OnGet()
+        {
+            string url = "/Login";
+
+            return Redirect(url);
+        }
+    }
+}
