106a324fd994cb3be8635d1a73b6c73a4c421490
Emil André Johannessen <emilaj@netcompany.com>
2021-10-14 19:14:23 +0200

    Added service for calculationg prices

---
+++ OceanicAirlines/Enums/DimensionCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace OceanicAirlines.Enums
+{
+    public enum DimensionCategory
+    {
+        A = 0,
+        B = 1,
+        C = 2
+    }
+}
---
+++ OceanicAirlines/Services/PriceCalculationService.cs
@@ -0,0 +1,25 @@
+﻿using OceanicAirlines.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OceanicAirlines.Services
+{
+    public class PriceCalculationService
+    {
+
+
+    }
+
+    private DimensionCategory GetDimensionGroup(double height, double weight, double depth)
+    {
+
+        var maxSizeCategoryA = 0.25;
+        var maxSizeCategoryB = 0.4;
+        var maxSizeCategoryC = 2;
+
+
+
+    }
+}
